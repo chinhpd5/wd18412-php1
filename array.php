842a380557e2bd205f876a84b0f86fcd3795742d
chinhpd5 <chinhpd5@fpt.edu.vn>
2023-11-08 14:04:40 +0700

    array

---
+++ array.php
@@ -0,0 +1,13 @@
+<?php
+    $array1 =[
+        "name" => "Phí Đức Chính",
+        "age" => 20
+        "khoa" => "Công nghệ thông tin",
+        "gt" => true,
+        "result" => [6,7.5,8.5,9]
+    ];
+
+    
+
+
+?>
