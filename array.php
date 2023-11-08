25a5ddf889a6ba44d5f47ee5d93036fa19610c37
chinhpd5 <chinhpd5@fpt.edu.vn>
2023-11-08 14:19:56 +0700

    array

--- array.php
+++ array.php
@@ -1,13 +1,42 @@
 <?php
-    $array1 =[
+    $array1 = [
         "name" => "Phí Đức Chính",
-        "age" => 20
+        "age" => 20,
         "khoa" => "Công nghệ thông tin",
         "gt" => true,
         "result" => [6,7.5,8.5,9]
     ];
 
-    
+    $array2 =[
+        [
+            "name" => "Phí Đức Chính",
+            "age" => 20,
+            "khoa" => "Công nghệ thông tin",
+            "gt" => true,
+            "result" => [6,7.5,8.5,9]
+        ],
+        [
+            "name" => "Đặng Thành Long",
+            "age" => 21,
+            "khoa" => "Ứng dụng phần mềm",
+            "gt" => true,
+            "result" => [5,9.5,10,9]
+        ],
+        [
+            "name" => "Trần Thị Ngọc",
+            "age" => 20,
+            "khoa" => "Lập trình web",
+            "gt" => false,
+            "result" => [5,8.5,9,10]
+        ],
+        [
+            "name" => "Đinh Ngọc Hồng",
+            "age" => 22,
+            "khoa" => "Ứng dụng phần mềm",
+            "gt" => false,
+            "result" => [4,6,8.5,10]
+        ]
+    ];
 
 
 ?>
